8779fba3200c174c56fb32039312ddc39e227f13
NicolasString04 <nicolinostringari@gmail.com>
2025-03-19 21:39:08 -0300

    teste responsividade celular

--- main.py
+++ main.py
@@ -2,17 +2,17 @@ from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
-# Rota para a página principal
+
 @app.route('/')
 def home():
     return render_template('main.html')
 
-# Rota para servir arquivos estáticos (CSS, imagens, etc.)
+
 @app.route('/static/<path:filename>')
 def static_files(filename):
     return app.send_static_file('static', filename)
 
-# Rota para servir os PDFs
+
 @app.route('/pdfs/<path:filename>')
 def pdf_files(filename):
     try:
